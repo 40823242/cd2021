c9d3c8c7347f875b6c4b18dafdc561faeefcc040
40823242 <40823242@gm.nfu.edu.tw>
2021-03-02 23:05:59 +0800

    03022

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '我的倉儲:https://github.com/40823242/cd2021', 'tags': '', 'url': 'About.html'}]};
